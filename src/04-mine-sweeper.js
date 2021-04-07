83ed0b187d702cc67959a9397731a86398192574
Eremeow138 <eremeow138@yandex.ru>
2021-04-07 15:41:48 +0800

    feat: add solution for 04-mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,60 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  // throw new Error('Not implemented');
+  const mlength = matrix.length;
+  const lenghOneArr = matrix[0].length;
+  const result = [];
+  matrix.forEach((item, index, array) => {
+    item.forEach((itm, ind, arr) => {
+      if (itm) {
+        result.push(1);
+      } else {
+        let counter = 0;
+        if (arr[ind - 1]) {
+          counter++;
+        }
+        if (arr[ind + 1]) {
+          counter++;
+        }
+        if (array[index - 1]) {
+          if (array[index - 1][ind - 1]) {
+            counter++;
+          }
+          if (array[index - 1][ind]) {
+            counter++;
+          }
+          if (array[index - 1][ind + 1]) {
+            counter++;
+          }
+        }
+        if (array[index + 1]) {
+          if (array[index + 1][ind - 1]) {
+            counter++;
+          }
+          if (array[index + 1][ind]) {
+            counter++;
+          }
+          if (array[index + 1][ind + 1]) {
+            counter++;
+          }
+        }
+        result.push(counter);
+      }
+    });
+  });
+  const final = [];
+  let counter = 0;
+  for (let i = 0; i < mlength; i++) {
+    const vector = [];
+    for (let j = 0; j < lenghOneArr; j++) {
+      vector.push(result[counter]);
+      counter++;
+    }
+    final.push(vector);
+  }
+  return final;
 }
 
 module.exports = minesweeper;
