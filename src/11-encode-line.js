77171a47c83fccc94b0b095c110732975b1eeb81
Eremeow138 <eremeow138@yandex.ru>
2021-04-08 17:44:44 +0800

    feat: add solution for 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,32 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  // throw new Error('Not implemented');
+  let result = '';
+  const stringArr = str.split('');
+  let counter = 1;
+  stringArr.forEach((item, index, arr) => {
+    if (arr[index + 1]) {
+      if (item === arr[index + 1]) {
+        counter++;
+      }
+      if (item !== arr[index + 1]) {
+        if (counter === 1) {
+          result += item;
+        } else {
+          result += counter + item;
+          counter = 1;
+        }
+      }
+    } else if (counter === 1) {
+      result += item;
+    } else {
+      result += counter + item;
+      counter = 1;
+    }
+  });
+  return result;
 }
 
 module.exports = encodeLine;
